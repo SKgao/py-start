ecbbb3006b6d31831a0b5c55f51ee95ab8c47a71
gaosikang <498039142@qq.com>
2019-04-28 16:53:45 +0800

    update dig

--- start-01/dig/Decorator.py
+++ start-01/dig/Decorator.py
@@ -5,3 +5,123 @@ from  IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = 'all'
 
 ################## Decorator 装饰器 ##################
+
+# 装饰器操作符 @
+
+def a_decorator(fun):
+    def warpper():
+        print('doing something here:')
+        fun()
+        print('well done!')
+    return warpper
+
+@ a_decorator
+def a_fun():
+    print('I`m coding...')
+a_fun()
+
+# 装饰器作用
+# 1.改变其他函数的行为
+# 2.装饰器类
+
+def uppercase(func):
+    def warpper():
+        result = func()
+        return result.upper()
+    return warpper
+
+def adddot(func):
+    def warpper():
+        result = func()
+        return f'{result} ..... the end'
+    return warpper
+
+def a_string():
+    return 'Learning python is very interesting'
+print(a_string())
+
+@uppercase
+def b_string():
+    return 'Learning python is very boring'
+print(b_string())
+
+@adddot
+@uppercase
+def c_string():
+    return 'Learning python Decorator'
+print(c_string()) # LEARNING PYTHON DECORATOR ..... the end
+
+@uppercase
+@adddot
+def d_string():
+    return 'Learning python Decorator'
+print(d_string()) # LEARNING PYTHON DECORATOR ..... THE END
+
+# 可见多个装饰器时，执行顺序为 至下朝上 ↑, 即为由内朝外
+
+
+# 装饰带有参数函数
+def lowercaer(func):
+    def warpper(*args):
+        result = func(*args)
+        return result.lower()
+    return warpper
+
+@lowercaer
+def e_string(str):
+    return f'writing here LOWER WORDS: {str}'
+print(e_string('MY WECHAT IS ZORO'))
+
+# 追溯 调用函数，及其传入的参数
+def trace(func):
+    def warpper(*args, **kwargs):
+        print(f"Trace: You called function: {func.__name__}()")
+        print(f"fn with args: {args} kwargs: {kwargs}")
+        original_result = func(*args, **kwargs)
+        print(f"Trace: {func.__name__} {args} returned: {original_result}")
+        return original_result
+    return warpper
+
+@trace
+def say_hi(greeting, name='ZORO'):
+    return f'{greeting}, {name} Nice to meet you'
+print(say_hi('Hi', name = 'John Snow'))
+
+
+# 类也可以用来构建装饰器
+from functools import wraps
+
+# functools，用于高阶函数：指那些作用于函数或者返回其它函数的函数
+# cmp_to_key，将一个比较函数转换关键字函数；
+# partial，针对函数起作用，并且是部分的；
+# reduce，与python内置的reduce函数功能一样；
+# total_ordering，在类装饰器中按照缺失顺序，填充方法；
+# update_wrapper，更新一个包裹（wrapper）函数，使其看起来更像被包裹（wrapped）的函数；
+# wraps，可用作一个装饰器，简化调用update_wrapper的过程；
+
+class logit(object):
+    def __init__(self, logfile='../../file/logout.txt'):
+        self.logfile = logfile
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            log_string = func.__name__ + " was called"
+            print(log_string, self.logfile)
+            # 打开logfile并写入
+            with open(self.logfile, 'a') as opened_file:
+                # 现在将日志打到指定的文件
+                opened_file.write(log_string + '\n')
+            # 现在，发送一个通知
+            self.notify()
+            return func(*args, **kwargs)
+        return wrapped_function
+
+    def notify(self):
+        # logit只打日志，不做别的
+        pass
+
+@logit()
+def logfunc():
+    print('log write succeed 0.0')
+logfunc()
--- start-01/dig/Generator.py
+++ start-01/dig/Generator.py
@@ -5,3 +5,46 @@ from  IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = 'all'
 
 ################## Generator 生成器 ##################
+
+# yield 语句
+def counter(start, end):
+    while start <= end:
+        yield start
+        start += 1
+
+for i in counter(1, 5):
+    print(i)
+# yield 语句与 return 最明显的不同在于，在它之后的语句依然会被执行
+# 生成器函数被 next() 调用后，执行到 yield 生成一个值返回（然后继续执行 next() 外部剩余的语句）；
+# 下次再被 next() 调用的时候，从上次生成返回值的 yield 语句处继续执行
+
+# 生成器表达式
+even = (e for e in range(10) if not e % 2)
+print('even::', even)
+for i in even:
+    print(i)
+
+# 表达式使用 (), 即是用生成器创造的迭代器
+# 表达式使用 [], 即是用生成器创造的列表
+# 表达式使用 {}, 即是用生成器创造的集合
+odd = [e for e in range(10, 20) if e % 2 == 1]
+print('odd::', odd)
+for i, v in enumerate(odd):
+    print(f'{i}-{v}')
+
+sum_of_odd = sum(s for s in range(20, 30) if s % 2)
+print('sum::', sum_of_odd)
+
+# 函数体嵌套
+def a_fun():
+    def b_fun():
+        print('this is b fun')
+    print('this is a fun')
+a_fun()
+
+def aa_fun():
+    def bb_fun():
+        print('this is b fun')
+    print('this is a fun')
+    return bb_fun()
+aa_fun()
