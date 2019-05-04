c53f7e455c608861992b382087e377f2c88e3aad
gsk <37265728+SKgao@users.noreply.github.com>
2019-05-04 17:09:39 +0800

    update

--- start/regular/pattern.py
+++ start/regular/pattern.py
@@ -3,5 +3,11 @@
 
 # from  IPython.core.interactiveshell import InteractiveShell
 # InteractiveShell.ast_node_interactivity = 'all'
+import re
 
 ################## 正则表达式 ##################
+
+str = 'The quick brown fox jumps over the lazy dog'
+pttn = re.compile(r'\wo\w')
+result = re.findall(pttn, str)
+print(result)
